17a21e2b3d3aaaab5e880f6f2f30dda9f7874a30
majskit <kmositdev@gmail.com>
2025-06-17 18:30:09 +0100

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,18 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
-};
-
-export default config;
+} satisfies Config;
